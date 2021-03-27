f836afaed41cd8c2035a89856f382c3b994899ef
Karen <karenchakhalyan23@gmail.com>
2021-03-27 19:46:30 +0400

    some changes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -28,11 +28,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 					onClick={() => {
 						setOpen(!open);
 					}}
-					className="ui selection dropdown visible active"
+					className={`ui selection dropdown ${open ? 'visible active' : ''}`}
 				>
 					<i className="dropdown icon"></i>
 					<div className="text">{selected.label}</div>
-					<div className="menu visible transition">{renderedOptions}</div>
+					<div className={`menu ${open ? 'visible transition' : ''}`}>
+						{renderedOptions}
+					</div>
 				</div>
 			</div>
 		</div>
