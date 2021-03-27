1aa802d56c32b73c34a36617bd5cbce1bdf98cf6
Karen <karenchakhalyan23@gmail.com>
2021-03-27 23:00:21 +0400

    some changes

---
+++ src/components/Convert.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Convert = ({ language, text }) => {
+	const [translated, setTranslated] = useState('');
+
+	useEffect(() => {
+		const doTranslation = async () => {
+			const { data } = await axios.post(
+				'https://translation.googleapis.com/language/translate/v2',
+				{},
+				{
+					params: {
+						q: text,
+						target: language.value,
+						key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+					},
+				}
+			);
+			setTranslated(data.data.translations[0].translatedText);
+		};
+		doTranslation();
+	}, [language, text]);
+	return (
+		<div>
+			<h2 className="ui header">{translated}</h2>
+		</div>
+	);
+};
+
+export default Convert;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Convert from './Convert';
 import Dropdown from './Dropdown';
 
 const options = [
@@ -18,32 +19,42 @@ const options = [
 		label: 'Russian',
 		value: 'ru',
 	},
+	{
+		label: 'Dutch',
+		value: 'nl',
+	},
 ];
 
 const Translate = () => {
 	const [language, setlanguage] = useState(options[0]);
-	const [text, settext] = useState('');
+	const [text, setText] = useState('');
 
 	return (
 		<div>
 			<div className="ui form">
 				<div className="field">
-					<div className="label">Enter text</div>
+					<label htmlFor="text" className="label">
+						Enter text
+					</label>
 					<input
+						name="text"
 						className="ui input"
 						type="text"
 						value={text}
-						onChange={(e) => settext(e.target.value)}
+						onChange={(e) => setText(e.target.value)}
 					/>
 				</div>
 			</div>
-
+			<br />
 			<Dropdown
 				label="Select a Language"
 				selected={language}
 				onSelectedChange={setlanguage}
 				options={options}
 			/>
+			<div className="ui divider"></div>
+			<h3 className="ui header">Output</h3>
+			<Convert language={language} text={text} />
 		</div>
 	);
 };
