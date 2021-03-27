95a914303e5b9aec661f1fb6e45904d202a676c3
Karen <karenchakhalyan23@gmail.com>
2021-03-28 00:56:14 +0400

    some changes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -37,21 +37,23 @@ const Dropdown = ({ label, options, selected, onSelectedChange }) => {
 	});
 
 	return (
-		<div ref={ref} className="ui form">
-			<div className="field">
-				<label className="label">{label}</label>
-				<div
-					onClick={() => setOpen(!open)}
-					className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-				>
-					<i className="dropdown icon"></i>
-					<div className="text">{selected.label}</div>
-					<div className={`menu ${open ? 'visible transition' : ''}`}>
-						{renderedOptions}
+		<>
+			<div ref={ref} className="ui form">
+				<div className="field">
+					<label className="label">{label}</label>
+					<div
+						onClick={() => setOpen(!open)}
+						className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+					>
+						<i className="dropdown icon"></i>
+						<div className="text">{selected.label}</div>
+						<div className={`menu ${open ? 'visible transition' : ''}`}>
+							{renderedOptions}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
