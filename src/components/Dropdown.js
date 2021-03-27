393615f1b22c5cdaf680330d6a997657d7fb8704
Karen <karenchakhalyan23@gmail.com>
2021-03-27 21:04:44 +0400

    some changes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,16 +5,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 	const ref = useRef();
 
 	useEffect(() => {
-		document.body.addEventListener(
-			'click',
-			(event) => {
-				if (ref.current && ref.current.contains(event.target)) {
-					return;
-				}
-				setOpen(false);
-			},
-			{ capture: true }
-		);
+		const onBodyClick = (event) => {
+			if (ref.current && ref.current.contains(event.target)) {
+				return;
+			}
+			setOpen(false);
+		};
+
+		document.body.addEventListener('click', onBodyClick);
+
+		return () => {
+			document.body.removeEventListener('click', onBodyClick);
+		};
 	}, []);
 
 	const renderedOptions = options.map((option) => {
